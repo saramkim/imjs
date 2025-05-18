4e10efd7a9b6d4e25fb49e312badb5fd3aa3dcf6
saramkim <saramkimm@gmail.com>
2025-05-19 01:31:04 +0900

    chore(init): set up project (Vite + React + TypeScript + Tailwind + Zustand + Monaco Editor)

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+/** @type {import("prettier").Config} */
+export default {
+  semi: true,
+  singleQuote: true,
+  trailingComma: "es5",
+  tabWidth: 2,
+  printWidth: 120,
+  bracketSpacing: true,
+};
