28d21b281dd157f7213295114d1c0f6df17a7082
saramkim <saramkimm@gmail.com>
2025-05-21 00:55:28 +0900

    feat(core): add ExecutionEngine to encapsulate interpreter and simulator

---
+++ src/core/execution-engine.ts
@@ -0,0 +1,44 @@
+import { Interpreter } from './interpreter/interpreter';
+import { Simulator } from './simulator/simulator';
+
+export class ExecutionEngine {
+  private interpreter = new Interpreter();
+  private simulator = new Simulator();
+
+  load(code: string) {
+    const commands = this.interpreter.interpret(code);
+    this.simulator.load(commands);
+  }
+
+  play() {
+    this.simulator.play();
+  }
+
+  pause() {
+    this.simulator.pause();
+  }
+
+  step() {
+    this.simulator.step();
+  }
+
+  reset() {
+    this.simulator.reset();
+  }
+
+  setSpeed(ms: number) {
+    this.simulator.setSpeed(ms);
+  }
+
+  getContext() {
+    return this.simulator.getContext();
+  }
+
+  getCurrentIndex() {
+    return this.simulator.getCurrentIndex();
+  }
+
+  getTotalCommands() {
+    return this.simulator.getTotalCommands();
+  }
+}
