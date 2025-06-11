ecede05c246fe0bc8befdc5cdc4bd302b098c9e3
saramkim <saramkimm@gmail.com>
2025-06-11 14:33:21 +0900

    feat(interpreter): refactor command generation to include call stack management with Push/Pop commands

--- src/core/interpreter/interpreter.ts
+++ src/core/interpreter/interpreter.ts
@@ -2,24 +2,28 @@ import * as acorn from 'acorn';
 import { Command } from '@core/command/command';
 import { ConsoleLogCommand } from '@core/command/console-log-command';
 import { SetTimeoutCommand } from '@core/command/set-timeout-command';
+import { PushCallStackCommand } from '@core/command/push-call-stack-command';
+import { PopCallStackCommand } from '@core/command/pop-call-stack-command';
 
 let commandId = 0;
 const nextId = () => `cmd_${commandId++}`;
 
 export class Interpreter {
   interpret(code: string): Command[] {
-    const ast = acorn.parse(code, {
-      ecmaVersion: 'latest',
-      locations: true,
-      sourceType: 'script',
-    });
+    const ast = this.parse(code);
+    const commands = this.generateCommands(ast, code);
 
+    return commands;
+  }
+
+  private generateCommands(ast: acorn.Program, code: string): Command[] {
     const commands: Command[] = [];
 
     for (const node of ast.body) {
       if (node.type !== 'ExpressionStatement') continue;
 
       const expr = node.expression;
+      const fnName = code.slice(expr.start, expr.end);
 
       if (
         expr.type === 'CallExpression' &&
@@ -31,7 +35,12 @@ export class Interpreter {
       ) {
         const arg = expr.arguments[0];
         if (arg?.type === 'Literal' && typeof arg.value === 'string') {
-          commands.push(new ConsoleLogCommand(arg.value, node.loc!));
+          const loc = node.loc!;
+          commands.push(
+            new PushCallStackCommand(fnName, loc),
+            new ConsoleLogCommand(arg.value, loc),
+            new PopCallStackCommand(loc)
+          );
         }
       } else if (
         expr.type === 'CallExpression' &&
@@ -43,11 +52,23 @@ export class Interpreter {
         const delay = delayArg?.type === 'Literal' && typeof delayArg.value === 'number' ? delayArg.value : 0;
 
         if (callback?.type === 'ArrowFunctionExpression') {
-          commands.push(new SetTimeoutCommand(nextId(), delay, node.loc!));
+          commands.push(
+            new PushCallStackCommand(fnName, node.loc!),
+            new SetTimeoutCommand(nextId(), delay, node.loc!),
+            new PopCallStackCommand(node.loc!)
+          );
         }
       }
     }
 
     return commands;
+  }
+
+  private parse(code: string): acorn.Program {
+    return acorn.parse(code, {
+      ecmaVersion: 'latest',
+      locations: true,
+      sourceType: 'script',
+    });
   }
 }
